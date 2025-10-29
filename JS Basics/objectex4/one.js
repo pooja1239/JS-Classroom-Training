7bf4b1806d7c78bf86610f630e121a3e7eaff7d2
pooja1239 <poojamustari43@gmail.com>
2025-10-29 19:27:51 +0530

    example

---
+++ JS Basics/objectex4/one.js
@@ -0,0 +1,14 @@
+let user = {}
+let emp = {
+    eid:101,
+    ename:"Rahul",
+    esal:45000
+}
+
+if(Object.keys(emp).lenght>0){
+    console.log("Not Empty Object")
+
+}
+else{
+    console.log("Empty Object")
+}
