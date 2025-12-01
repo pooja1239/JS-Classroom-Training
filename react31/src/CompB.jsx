38f4a3934aa7bb454e2a9abc372d99801a809dbc
pooja1239 <poojamustari43@gmail.com>
2025-12-01 13:06:55 +0530

    cs

--- react31/src/CompB.jsx
+++ react31/src/CompB.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from 'react';
 
-const CompB =(props)=>{
-    return (
-        <div>
-            <h4>Component B</h4>
-            <pre>{JSON.stringify(props)}</pre>
-            
-        </div>
-    )
-}
+const CompB = (props) => {
+  let { greet, emp_Id, employees } = props;  // Object Destructuring
+
+  return (
+    <div>
+      <h3>Component B</h3>
+
+      <h3>{JSON.stringify(props)}</h3>
+      <h4>{greet()}</h4>
+      <h4>{emp_Id}</h4>
+
+      <table border={3}>
+        <thead>
+          <tr>
+            <th>Emp Id</th>
+            <th>Name</th>
+            <th>Gender</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {employees.map((emp) => {
+            return (
+              <tr> 
+                <td>{emp.eid}</td>
+                <td>{emp.ename}</td>
+                <td>{emp.gender}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 export default CompB;
